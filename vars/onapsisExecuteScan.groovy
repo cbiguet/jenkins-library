19b0e5938bfb640855f317b25bcb318c1916c60c
Constantin Biguet <cbiguet@onapsis.com>
2024-10-18 10:59:38 -0400

    Add checkScript pre-requisite

--- vars/onapsisExecuteScan.groovy
+++ vars/onapsisExecuteScan.groovy
@@ -1,19 +1,11 @@
-import com.cloudbees.groovy.cps.NonCPS
-import com.sap.piper.Utils
 import groovy.transform.Field
+import static com.sap.piper.Prerequisites.checkScript
 
 @Field String STEP_NAME = getClass().getName()
 @Field String METADATA_FILE = 'metadata/onapsisExecuteScan.yaml'
 
-/**
- * Name of library step
- *
- * @param script global script environment of the Jenkinsfile run
- * @param others document all parameters
- */
 def call(Map parameters = [:], body) {
-    handlePipelineStepErrors (stepName: STEP_NAME, stepParameters: parameters) {
-        List credentials = [[type: 'token', id: 'onapsisCredentialsId', , env: ['PIPER_token']]]
-        piperExecuteBin(parameters, STEP_NAME, METADATA_FILE, credentials)
-    }
+    final script = checkScript(this, parameters) ?: this
+    List credentials = [[type: 'token', id: 'onapsisCredentialsId', env: ['PIPER_token']]]
+    piperExecuteBin(parameters, STEP_NAME, METADATA_FILE, credentials)
 }
