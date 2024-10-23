24eed21d565c899666ecf5a97dcaea02b3e5666d
cbiguet <constantin.biguet@gmail.com>
2024-10-22 21:20:49 -0400

    Cleanup

--- cmd/onapsisExecuteScan.go
+++ cmd/onapsisExecuteScan.go
@@ -41,71 +41,16 @@ type onapsisExecuteScanUtilsBundle struct {
 	// onapsisExecuteScanUtilsBundle and forward to the implementation of the dependency.
 }
 
-// func zipProject(folderPath string, outputPath string) error {
-// 	// Create the output file
-// 	zipFile, err := os.Create(outputPath)
-// 	if err != nil {
-// 		return fmt.Errorf("failed to create zip file: %w", err)
-// 	}
-// 	defer zipFile.Close()
-
-// 	// Create a new zip writer
-// 	zipWriter := zip.NewWriter(zipFile)
-// 	defer zipWriter.Close()
-
-// 	// Walk through all the files in the folder
-// 	err = filepath.Walk(folderPath, func(path string, info os.FileInfo, err error) error {
-// 		if err != nil {
-// 			return err
-// 		}
-
-// 		// Create a header based on the file info
-// 		header, err := zip.FileInfoHeader(info)
-// 		if err != nil {
-// 			return err
-// 		}
-
-// 		// Ensure the correct relative file path in the zip
-// 		header.Name, err = filepath.Rel(filepath.Dir(folderPath), path)
-// 		if err != nil {
-// 			return err
-// 		}
-
-// 		if info.IsDir() {
-// 			header.Name += "/"
-// 		} else {
-// 			header.Method = zip.Deflate
-// 		}
-
-// 		// Create the writer for this file
-// 		writer, err := zipWriter.CreateHeader(header)
-// 		if err != nil {
-// 			return err
-// 		}
-
-// 		// If it's a file, copy the content into the zip
-// 		if !info.IsDir() {
-// 			file, err := os.Open(path)
-// 			if err != nil {
-// 				return err
-// 			}
-// 			defer file.Close()
-
-// 			_, err = io.Copy(writer, file)
-// 			if err != nil {
-// 				return err
-// 			}
-// 		}
-
-// 		return nil
-// 	})
-
-// 	if err != nil {
-// 		return fmt.Errorf("failed to zip folder: %w", err)
-// 	}
-
-// 	return nil
-// }
+func newOnapsisExecuteScanUtils() onapsisExecuteScanUtils {
+	utils := onapsisExecuteScanUtilsBundle{
+		Command: &command.Command{},
+		Files:   &piperutils.Files{},
+	}
+	// Reroute command output to logging framework
+	utils.Stdout(log.Writer())
+	utils.Stderr(log.Writer())
+	return &utils
+}
 
 var includePatterns = []string{
 	"**/*.js",
@@ -231,102 +176,6 @@ func zipProject(folderPath string, outputPath string) error {
 	return nil
 }
 
-// func zipProject(folderPath string, outputPath string) error {
-// 	log.Entry().Infof("Starting to zip folder: %s", folderPath)
-
-// 	// Create the output file
-// 	zipFile, err := os.Create(outputPath)
-// 	if err != nil {
-// 		return fmt.Errorf("failed to create zip file: %w", err)
-// 	}
-// 	defer zipFile.Close()
-
-// 	log.Entry().Infof("Created zip file: %s", outputPath)
-
-// 	// Create a new zip writer
-// 	zipWriter := zip.NewWriter(zipFile)
-// 	defer zipWriter.Close()
-
-// 	// Track file count
-// 	fileCount := 0
-
-// 	// Walk through all the files in the folder
-// 	err = filepath.Walk(folderPath, func(path string, info os.FileInfo, err error) error {
-// 		if err != nil {
-// 			log.Entry().Errorf("Error accessing path %s: %v", path, err)
-// 			return err
-// 		}
-
-// 		// Log each file being processed
-// 		log.Entry().Infof("Zipping file or directory: %s", path)
-
-// 		// Create a header based on the file info
-// 		header, err := zip.FileInfoHeader(info)
-// 		if err != nil {
-// 			log.Entry().Errorf("Failed to create zip header for file: %s", path)
-// 			return err
-// 		}
-
-// 		// Ensure the correct relative file path in the zip
-// 		header.Name, err = filepath.Rel(filepath.Dir(folderPath), path)
-// 		if err != nil {
-// 			log.Entry().Errorf("Failed to create relative path for file: %s", path)
-// 			return err
-// 		}
-
-// 		if info.IsDir() {
-// 			header.Name += "/"
-// 		} else {
-// 			header.Method = zip.Deflate
-// 		}
-
-// 		// Create the writer for this file
-// 		writer, err := zipWriter.CreateHeader(header)
-// 		if err != nil {
-// 			log.Entry().Errorf("Failed to write header for file: %s", path)
-// 			return err
-// 		}
-
-// 		// If it's a file, copy the content into the zip
-// 		if !info.IsDir() {
-// 			file, err := os.Open(path)
-// 			if err != nil {
-// 				log.Entry().Errorf("Failed to open file: %s", path)
-// 				return err
-// 			}
-// 			defer file.Close()
-
-// 			_, err = io.Copy(writer, file)
-// 			if err != nil {
-// 				log.Entry().Errorf("Failed to copy file content to zip for file: %s", path)
-// 				return err
-// 			}
-// 		}
-
-// 		fileCount++
-// 		return nil
-// 	})
-
-// 	if err != nil {
-// 		return fmt.Errorf("failed to zip folder: %w", err)
-// 	}
-
-// 	log.Entry().Infof("Successfully zipped %d files", fileCount)
-
-// 	return nil
-// }
-
-func newOnapsisExecuteScanUtils() onapsisExecuteScanUtils {
-	utils := onapsisExecuteScanUtilsBundle{
-		Command: &command.Command{},
-		Files:   &piperutils.Files{},
-	}
-	// Reroute command output to logging framework
-	utils.Stdout(log.Writer())
-	utils.Stderr(log.Writer())
-	return &utils
-}
-
 func onapsisExecuteScan(config onapsisExecuteScanOptions, telemetryData *telemetry.CustomData) {
 	// Utils can be used wherever the command.ExecRunner interface is expected.
 	// It can also be used for example as a mavenExecRunner.
@@ -347,8 +196,6 @@ func onapsisExecuteScan(config onapsisExecuteScanOptions, telemetryData *telemet
 }
 
 func runOnapsisExecuteScan(config *onapsisExecuteScanOptions, telemetryData *telemetry.CustomData, utils onapsisExecuteScanUtils) error {
-	log.Entry().WithField("LogField", "Log field content").Info("This is just a demo for a simple step.")
-
 	// Create a new ScanServer
 	log.Entry().Info("Creating scan server...")
 	server, err := NewScanServer(&piperHttp.Client{}, config.ScanServiceURL, config.AccessToken)
@@ -364,20 +211,6 @@ func runOnapsisExecuteScan(config *onapsisExecuteScanOptions, telemetryData *tel
 	}
 	// Log the JobID
 	log.Entry().Infof("JobID: %s", response.Result.JobID)
-
-	// // Example of calling methods from external dependencies directly on utils:
-	// exists, err := utils.FileExists("file.txt")
-	// if err != nil {
-	// 	// It is good practice to set an error category.
-	// 	// Most likely you want to do this at the place where enough context is known.
-	// 	log.SetErrorCategory(log.ErrorConfiguration)
-	// 	// Always wrap non-descriptive errors to enrich them with context for when they appear in the log:
-	// 	return fmt.Errorf("failed to check for important file: %w", err)
-	// }
-	// if !exists {
-	// 	log.SetErrorCategory(log.ErrorConfiguration)
-	// 	return fmt.Errorf("cannot run without important file")
-	// }
 
 	return nil
 }
@@ -406,20 +239,6 @@ func NewScanServer(client piperHttp.Uploader, serverUrl string, token string) (*
 }
 
 func (srv *ScanServer) ScanProject(config *onapsisExecuteScanOptions, telemetryData *telemetry.CustomData, utils onapsisExecuteScanUtils, language string) (Response, error) {
-	// // Zip workspace files
-	// zipFile, err := zipProject(utils)
-	// if err != nil {
-	// 	return Response{}, errors.Wrap(err, "failed to zip workspace files")
-	// }
-
-	// // Get zip file content
-	// file := zipFile.Name()
-	// fileHandle, err := utils.Open(file)
-	// if err != nil {
-	// 	return Response{}, errors.Wrapf(err, "unable to locate file %v", file)
-	// }
-	// defer fileHandle.Close()
-
 	// Get workspace path
 	log.Entry().Info("Getting workspace path...") // DEBUG
 	workspace, err := utils.Getwd()
@@ -506,21 +325,22 @@ func (srv *ScanServer) ScanProject(config *onapsisExecuteScanOptions, telemetryD
 
 }
 
-// func zipWorkspace(utils onapsisExecuteScanUtils) (*os.File, error) {
-// 	zipFilePath := filepath.Join(utils.GetWorkspace(), "workspace.zip")
-// 	patterns := piperutils.Trim(strings.Split(filterPattern, ","))
-// 	sort.Strings(patterns)
-// 	zipFile, err := os.Create(zipFilePath)
-// 	if err != nil {
-// 		return zipFile, errors.Wrap(err, "failed to create archive of project sources")
-// 	}
-// 	defer zipFile.Close()
-// 	err = zipFolder(utils.GetWorkspace(), zipFile, patterns, utils)
-// 	if err != nil {
-// 		return nil, errors.Wrap(err, "failed to compact folder")
-// 	}
-// 	return zipFile, nil
-// }
+func (srv *ScanServer) GetScanJobStatus(jobID string) (Response, error) {
+	// Send request
+	response, err := srv.client.SendRequest("GET", srv.serverUrl+"/cca/v1.2/job/"+jobID, nil, nil, nil)
+	if err != nil {
+		return Response{}, errors.Wrap(err, "failed to send request")
+	}
+
+	// Parse response
+	responseData := Response{}
+	err = piperHttp.ParseHTTPResponseBodyJSON(response, &responseData)
+	if err != nil {
+		return Response{}, errors.Wrap(err, "failed to parse response")
+	}
+
+	return responseData, nil
+}
 
 type Response struct {
 	Success bool             `json:"success"`
